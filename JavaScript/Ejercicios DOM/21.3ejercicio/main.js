1a3a817a90f25fba48b6663dc504c78074843300
eduweb1966 <eduardorodriguezmacassi@gmail.com>
2023-07-12 14:21:34 +0200

    commit ejercicios

--- JavaScript/Ejercicios DOM/21.3ejercicio/main.js
+++ JavaScript/Ejercicios DOM/21.3ejercicio/main.js
@@ -16,6 +16,53 @@ console.log(countries)
 const elemento = document.querySelector('.fn-remove-me');
 elemento.remove();
 
-//1.3 Utiliza el array para crear dinamicamente una lista ul > li de elementosen el div de html con el atributo data-function="printHere".
+//1.3 Utiliza el array para crear dinamicamente una lista ul > li de elementosen el div de html con el atributo data-function ="printHere".
 
 const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];
+
+const printHereDiv = document.querySelector('[data-function ="printHere"]');
+const ul = document.createElement('ul');
+
+cars.forEach(cars => {
+  const li = document.createElement('li');
+  li.textContent = cars;
+  ul.appendChild(li);
+});
+printHereDiv.appendChild(ul);
+console.log(cars)
+
+// 1.4 Crea dinamicamente en el html una serie de divs que contenga un elemento
+//h4 para el titulo y otro elemento img para la imagen.
+//const countries = [
+//{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=1'},
+//{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=2'},
+//{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=3'},
+//{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=4'},
+//{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5'}
+//];
+
+const countries2 = [
+    {title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=1'},
+    {title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=2'},
+    {title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=3'},
+    {title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=4'},
+    {title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5'},
+  ];
+  
+  countries2.forEach(country => {
+
+    const div = document.createElement('div');
+    const h4 = document.createElement('h4');
+    h4.textContent = country.title;
+  
+    const img = document.createElement('img');
+    img.src = country.imgUrl;
+    img.alt = country.title;
+    div.appendChild(h4);
+    div.appendChild(img);
+ 
+    document.querySelector('[data-function="printHere"]').appendChild(div);
+  });
+  console.log(countries)
+
+ 
