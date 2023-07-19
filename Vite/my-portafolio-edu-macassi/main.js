1cffd09964be5c399b0bf93c807294bb9a0f7610
eduweb1966 <eduardorodriguezmacassi@gmail.com>
2023-07-19 14:27:41 +0200

    final de clase

--- Vite/my-portafolio-edu-macassi/main.js
+++ Vite/my-portafolio-edu-macassi/main.js
@@ -4,7 +4,7 @@ import { linkPage } from "./utils/linkPage";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Footer } from "./components/Footer/Footer";
 import { Home } from "./pages/Home/Home";
-import { Projects } from "./components/";
+import { Projects } from "./pages/Projects/Projects";
 import { Divider } from "./components/Divider/Divider";
 const header = document.querySelector("header");
 header.innerHTML = Navbar();
