829dfb9ba3674886d608e10e2fa4ec1fc7664404
eduweb1966 <eduardorodriguezmacassi@gmail.com>
2023-07-19 13:00:08 +0200

    cambios

--- Vite/my-portafolio-edu-macassi/main.js
+++ Vite/my-portafolio-edu-macassi/main.js
@@ -5,7 +5,7 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { Footer } from "./components/Footer/Footer";
 import { Home } from "./pages/Home/Home";
 import { Projects } from "./pages/Projects/Projects";
-import { Divider } from "./components/Divider/Divider";
+import { Divider } from "./components/Divider/Divider ";
 const header = document.querySelector("header");
 header.innerHTML = Navbar();
 const footer = document.querySelector("footer");
