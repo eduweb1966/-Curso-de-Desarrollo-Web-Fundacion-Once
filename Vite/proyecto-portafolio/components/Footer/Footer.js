9fdbd2865b420d9281192b8b447da9c88d491a75
eduweb1966 <eduardorodriguezmacassi@gmail.com>
2023-07-25 00:59:36 +0200

    commit 25 de julio

--- Vite/proyecto-portafolio/components/Footer/Footer.js
+++ Vite/proyecto-portafolio/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import "./Footer.css";
 import { Button } from "../Button/Button";
 export const Footer = () => `
-<h2>Contact</h2>
+<h2>Contacto</h2>
 <div>
 ${Button("/icons/twitter.png", "Twitter")}
 ${Button("/icons/github.png", "GitHub")}
--- Vite/proyecto-portafolio/components/ProjectCard/ProjectCard.js
+++ Vite/proyecto-portafolio/components/ProjectCard/ProjectCard.js
@@ -13,11 +13,12 @@ export const ProjectCard = (project) => `
 </a>
 <a href=${project.twitter}>
 <img src="/icons/twitter.png" alt="Twitter icon" />
-</a><a href=${telegrams.link}>
+</a>
+<a href=${telegrams.link}>
 <img src="/icons/telegram.png" alt="Telegram icon" />
 </a>
 </div>
-</div>
+<div>
 <div class="detail">
 <p>${project.description}</p>
 <p class="tech">${project.tech.join(" - ")}</p>
