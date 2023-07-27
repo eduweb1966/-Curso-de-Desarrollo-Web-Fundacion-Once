11eba77008d1147220f627aa00fc84f151c54bfa
eduweb1966 <eduardorodriguezmacassi@gmail.com>
2023-07-27 12:22:19 +0200

    commit al 27

--- JavaScript/Ejercicios_JavaScript/22.ejercicio4.js
+++ JavaScript/Ejercicios_JavaScript/22.ejercicio4.js
@@ -1,14 +1,23 @@
 //1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
 const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+desarrollo:
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+const hulk = avengers[0];
+console.log(hulk); // Devolverá "HULK"
+
 console.log(avengers[0]);
+
 //1.2 Cambia el primer elemento de avengers a "IRONMAN"
 avengers[0] = "IRONMAN";
+
 //1.3 console numero de elementos en el array usando la propiedad correcta de Array.
 console.log(avengers.length);
 
 const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
 //1.4 Añade 2 elementos al array: "Morty" y "Summer".
 rickAndMortyCharacters.push("Morty", "Summer");
+
 //Muestra en consola el último personaje del array
 console.log(rickAndMortyCharacters[rickAndMortyCharacters.length - 1]);
 
