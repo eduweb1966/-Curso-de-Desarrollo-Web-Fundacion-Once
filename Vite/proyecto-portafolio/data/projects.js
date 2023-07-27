beb6c800b48db8880f9518422f00ba24ee6002de
eduweb1966 <eduardorodriguezmacassi@gmail.com>
2023-07-27 11:13:25 +0200

    cambios del proyecto portafolio

--- Vite/proyecto-portafolio/data/projects.js
+++ Vite/proyecto-portafolio/data/projects.js
@@ -4,73 +4,57 @@ export const projects = [
         description: "Muestra variada de instrumentos Andinos",
         image:
             "https://res.cloudinary.com/dmklwicfb/image/upload/v1690282171/inmstrumentos_musicales_andinos_anoa6l.jpg",
-        tech: ["React", "HTML5", "CSS3", "JavaScript"],
-        github: "http://github.com/myuser/proyecto1",
-        link: "www.proyecto1.com",
+        github: "https://github.com/eduweb1966/-Curso-de-Desarrollo-Web-Fundacion-Once/tree/main/Vite/my-portafolio-edu-macassi",
     },
     {
         title: "Instrumentos de Cuerda",
         description: "Muestra variada de Instrumentos de Cuerda",
         image:
             "https://res.cloudinary.com/dmklwicfb/image/upload/v1690282012/instrumentos_de_cuerda_y9eyac.jpg",
-        tech: ["React", "HTML5", "CSS3", "JavaScript"],
-        github: "http://github.com/myuser/proyecto2",
-        link: "www.proyecto1.com",
+        github: "https://github.com/eduweb1966/-Curso-de-Desarrollo-Web-Fundacion-Once/tree/main/Vite/my-portafolio-edu-macassi",
     },
     {
         title: "Instrumentos Musicales de India",
-        description: "Muetra Variada de Instrumentos Musicales de India",
+        description: "Muestra Variada de Instrumentos Musicales de India",
         image:
             "https://res.cloudinary.com/dmklwicfb/image/upload/v1690282011/instrumentos_musicales_de_India_psea6g.jpg",
-        tech: ["React", "HTML5", "CSS3", "JavaScript"],
-        github: "http://github.com/myuser/proyecto3",
-        link: "www.proyecto1.com",
+        github: "https://github.com/eduweb1966/-Curso-de-Desarrollo-Web-Fundacion-Once/tree/main/Vite/my-portafolio-edu-macassi",        
     },
     {
         title: "Instrumentos de Percusion",
-        description: "Muetra Variada de Instrumentos Musicales de Precusión",
+        description: "Muestra Variada de Instrumentos Musicales de Precusión",
         image:
             "https://res.cloudinary.com/dmklwicfb/image/upload/v1690282011/instruymnetos_de_percusion_dmlr7z.jpg",
-        tech: ["React", "HTML5", "CSS3", "JavaScript"],
-        github: "http://github.com/myuser/proyecto4",
-        link: "www.proyecto1.com",
+        github: "https://github.com/eduweb1966/-Curso-de-Desarrollo-Web-Fundacion-Once/tree/main/Vite/my-portafolio-edu-macassi",
     },
     {
         title: "Instrumentos de Viento",
-        description: "Muetra Variada de Instrumentos Musicales de Viento",
+        description: "Muestra Variada de Instrumentos Musicales de Viento",
         image:
             "https://res.cloudinary.com/dmklwicfb/image/upload/v1690282011/instrumentos_de_viento_mob1u0.jpg",
-        tech: ["React", "HTML5", "CSS3", "JavaScript"],
-        github: "http://github.com/myuser/proyecto5",
-        link: "www.proyecto1.com",
+        github: "https://github.com/eduweb1966/-Curso-de-Desarrollo-Web-Fundacion-Once/tree/main/Vite/my-portafolio-edu-macassi",
     },
     {
         title: "Instrumentos Musicales Arabicos",
-        description: "Muetra Variada de Instrumentos de uso en la cultura arabica",
+        description: "Muetra Variada de Instrumentos de uso en la Cultura Arabica",
         image:
             "https://tse1.mm.bing.net/th?id=OIP.NkpLiuN_F-hVceY_U6VqxAHaFG&pid=Api&P=0&h=180",
-        tech: ["React", "HTML5", "CSS3", "JavaScript"],
-        github: "http://github.com/myuser/proyecto5",
-        link: "www.proyecto1.com",
+            github: "https://github.com/eduweb1966/-Curso-de-Desarrollo-Web-Fundacion-Once/tree/main/Vite/my-portafolio-edu-macassi",
     },
 
     {
         title: "Instrumentos Musicales Persas",
-        description: "Muetra Variada de Instrumentos de uso en la cultura Persa",
+        description: "Muestra Variadad de Instrumentos de uso en la cultura Persa",
         image:
             "https://tse4.mm.bing.net/th?id=OIP.RNIE5QTjuJiETvyGCNQc_gHaFj&pid=Api&P=0&h=180",
-        tech: ["React", "HTML5", "CSS3", "JavaScript"],
-        github: "http://github.com/myuser/proyecto5",
-        link: "www.proyecto1.com",
+            github: "https://github.com/eduweb1966/-Curso-de-Desarrollo-Web-Fundacion-Once/tree/main/Vite/my-portafolio-edu-macassi",
     },
     {
         title: "Instrumentos Musicales del Continente Australiano",
-        description: "Muetra Variada de Instrumentos de uso en el Continente Australiano",
+        description: "Muestra Variadad de Instrumentos de uso en el Continente Australiano",
         image:
             "https://tse4.mm.bing.net/th?id=OIP.RNIE5QTjuJiETvyGCNQc_gHaFj&pid=Api&P=0&h=180",
-        tech: ["React", "HTML5", "CSS3", "JavaScript"],
-        github: "http://github.com/myuser/proyecto5",
-        link: "www.proyecto1.com",
+            github: "https://github.com/eduweb1966/-Curso-de-Desarrollo-Web-Fundacion-Once/tree/main/Vite/my-portafolio-edu-macassi",
     },
 
 
