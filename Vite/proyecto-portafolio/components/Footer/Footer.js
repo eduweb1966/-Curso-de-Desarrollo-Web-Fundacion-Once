50a9e39f94886a8b37d95d33583268368230a512
eduweb1966 <eduardorodriguezmacassi@gmail.com>
2023-07-27 13:16:44 +0200

    commit

--- Vite/proyecto-portafolio/components/Footer/Footer.js
+++ Vite/proyecto-portafolio/components/Footer/Footer.js
@@ -1,12 +1,15 @@
 import "./Footer.css";
 import { Button } from "../Button/Button";
 export const Footer = () => `
-<h2></h2>
+<h2>
  
 <a href="mailto:erodriguezmacassi@gmail.com"><button class="button">contacto</button></a>
 <div>
+</h2>
 ${Button("/icons/twitter.png", "Twitter")}
 ${Button("/icons/github.png", "GitHub")}
 ${Button("/icons/linkeding.png", "LinkedIn")}
+${Button("/icons/instagram.png", "Intagram")}
+${Button("/icons/facebook.png", "Facebook")}
 </div>
 `;
