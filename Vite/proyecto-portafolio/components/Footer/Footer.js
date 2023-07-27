8fffd08705fc2b1eaebc983289447328de81e2a7
eduweb1966 <eduardorodriguezmacassi@gmail.com>
2023-07-27 11:41:13 +0200

    commit

--- Vite/proyecto-portafolio/components/Footer/Footer.js
+++ Vite/proyecto-portafolio/components/Footer/Footer.js
@@ -2,7 +2,8 @@ import "./Footer.css";
 import { Button } from "../Button/Button";
 export const Footer = () => `
 <h2></h2>
-<a href="mailto:erodriguezmacassi@gmail.com">Contacto -></a>
+ 
+<a href="mailto:erodriguezmacassi@gmail.com"><button class="button">contacto</button></a>
 <div>
 ${Button("/icons/twitter.png", "Twitter")}
 ${Button("/icons/github.png", "GitHub")}
--- Vite/proyecto-portafolio/pages/Home/Home.js
+++ Vite/proyecto-portafolio/pages/Home/Home.js
@@ -7,6 +7,6 @@ main.innerHTML = `
 <section class="home">
 <p>Portafolio de :</p>
 <h1>Eduardo Antonio Rodriguez Macassi</h>
-<p>Este es un Trabajo para es el para el Curso de Desarrollo Web y Trata de Mostrar de Equipos Musicales de Todo Tipo</p>
+<p>Este es un Trabajo para es el para el Curso de Desarrollo Web y Trata de Mostrar de Equipos Musicales de Todo el Mundo</p>
 </section>`;
 };
