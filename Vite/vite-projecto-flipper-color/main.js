fe8f24d8070cbf52c5ceb69566fc27f6a57bb2ee
eduweb1966 <eduardorodriguezmacassi@gmail.com>
2023-07-27 09:32:37 +0200

    commit jueves 27

--- Vite/vite-projecto-flipper-color/main.js
+++ Vite/vite-projecto-flipper-color/main.js
@@ -1,40 +1,63 @@
-import './style.css'
+ import './style.css';
 const COLOR_PALETTE = {
-'#7A7978': 'Gray',
-'#A80874': 'Fandango',
-'#B7FDFE': 'Celeste',
-'#480355': 'Phlocks',
-'#FF8C42': 'Pumpkin'
+
+  '#28262C': 'Raisin Black',
+
+  '#998FC7': 'Blue Bell',
+
+  '#D4C2FC': 'Lavender Blue',
+
+  '#F9F5FF': 'Magnolia',
+
+  '#14248A': 'Resolution Blue'
 };
-Object.keys(COLOR_PALETTE).forEach((color) => {
-const option = document.createElement("option");
-option.value = color;
-option.innerText = COLOR_PALETTE[color];
-});
 
-const colorPickerSelect = document.querySelector("#color-picker");
-Object.keys(COLOR_PALETTE).forEach((color) => {
-const option = document.createElement("option");
-option.value = color;
-option.innerText = COLOR_PALETTE[color];
-colorPickerSelect.append(option);
-})
+ 
+
+  const colorPickerSelect = document.querySelector("#color-picker");
+  
+   
+  
+  Object.keys(COLOR_PALETTE).forEach((color) => {
+  
+    const option = document.createElement("option");
+  
+    option.value = color;
+  
+    option.innerText = COLOR_PALETTE[color];
+  
+    colorPickerSelect.append(option);
+  
+  });
+  
+   
+  
   const addEventListenerToColorPicker = () => {
+  
     const colorPickerSelect = document.querySelector("#color-picker");
+  
     const colorName = document.querySelector("#color-name");
+  
+   
+  
     colorPickerSelect.addEventListener("change", (event) => {
-    const newColor = event.target.value;
-    document.body.style.backgroundColor = newColor;
-    //Le asignamos a nuestro h2 el valor de la clave que hay almacenada en newColor al igual que hemos hecho en la primera función
-    colorName.innerText = COLOR_PALETTE[newColor];
+  
+      const newColor = event.target.value;
+  
+      document.body.style.backgroundColor = newColor;
+  
+   
+  
+      const colorNameText = `${COLOR_PALETTE[newColor]} | ${newColor}`;
+  
+      colorName.innerText = COLOR_PALETTE[newColor] ? colorNameText : "-";
+  
     });
-    };
-
-    addOptionsToColorPicker();
-
-    addEventListenerToColorPicker();
-
-    const colorNameText = `${COLOR_PALETTE[newColor]} | ${newColor}`
-    colorName.innerText = COLOR_PALETTE[newColor] ? colorNameText : "-";
-
+  
+  };
+  
+   
+  
+  addEventListenerToColorPicker();
+  
  
